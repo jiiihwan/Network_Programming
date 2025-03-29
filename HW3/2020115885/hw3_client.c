c8a65b98af774dd45c8480836589b3ec1e95f348
jihwan <yjh2353693@naver.com>
2025-03-29 22:18:16 +0900

    1차 커밋

--- HW3/2020115885/hw3_client.c
+++ HW3/2020115885/hw3_client.c
@@ -11,13 +11,14 @@ int main(int argc, char** argv) {
     char buf[MAX];
     struct sockaddr_in servaddr;
     int opCount, opResult;
+    socklen_t len; //주소 길이 전달용 변수추가
 
     if(argc < 3) {
         printf("usage: ./client address port");
         return -1;
     }
 
-    if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if((sfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socekt error");
         return -1;
     }
@@ -26,19 +27,13 @@ int main(int argc, char** argv) {
     servaddr.sin_addr.s_addr = inet_addr(argv[1]);
     servaddr.sin_port = htons(atoi(argv[2]));
 
-    if(connect(sfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("connect error");
-        return -1;
-    }
-
-    //puts("Conncted....");
-
     printf("Operand count: ");
     scanf("%d", &opCount); //operation Count를 입력받는다
     buf[0] = (char)opCount; //buf배열 첫번째 인덱스에 opCount를 char형태로 저장
 
     if(buf[0] <= 0) { //opCount가 음수면 소켓에 buf내용을 보내고 서버소켓 종료
-        write(sfd, buf, 1);
+        //write(sfd, buf, 1);
+        sendto(sfd, buf, 1, 0, (struct sockaddr*)&servaddr, sizeof(servaddr));
         close(sfd);
         return 0;
     }
@@ -53,9 +48,13 @@ int main(int argc, char** argv) {
         scanf(" %c", &buf[(opCount*4)+1+i]); //operator를 buf배열의 숫자개수x4+1+i 인덱스에 opCount입력받기(buf마지막에 operator들 넣기)
     }
 
-    write(sfd, buf, (opCount*4)+(opCount-1)*4); //server소켓에 buf의 데이터를 전달
+    len = sizeof(servaddr);
 
-    read(sfd, &opResult, 4); //server소켓에서 데이터를 읽어와서 opResult에 저장
+    //write(sfd, buf, (opCount*4)+(opCount-1)*4); //server소켓에 buf의 데이터를 전달
+    sendto(sfd, buf, (opCount*4)+(opCount-1)*4, 0, (struct sockaddr*)&servaddr, len);
+
+    //read(sfd, &opResult, 4); //server소켓에서 데이터를 읽어와서 opResult에 저장
+    recvfrom(sfd, &opResult, 4, 0, (struct sockaddr*)&servaddr, &len);
     printf("Operation result: %d\n", opResult);
 
     close(sfd);
--- HW3/2020115885/hw3_server.c
+++ HW3/2020115885/hw3_server.c
@@ -7,7 +7,7 @@
 #define MAX 1024
 
 int main(int argc, char** argv) {
-    int sfd, cfd;
+    int sfd; //UDP에서는 sfd만 존재
     struct sockaddr_in servaddr, cliaddr;
     socklen_t len;
     char opCount;
@@ -20,13 +20,13 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if((sfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socket error");
         return 1;
     }
 
     int enable = 1;
-    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)); //소켓 재사용할 수 있게 하는 옵션
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -37,18 +37,11 @@ int main(int argc, char** argv) {
         return 1;
     }       
 
-    if(listen(sfd, 5) < 0) {
-        perror("listen error");
-        return 1;
-    }
+    len = sizeof(cliaddr);
 
     while(1){
-        if((cfd = accept(sfd, (struct sockaddr *)&cliaddr, &len)) < 0) {
-            perror("accept error");
-            return 1;
-        }
-
-        read(cfd, &opCount, 1); //cfd로부터 최대 1바이트만 읽어서 opCount변수에 저장한다
+        //read(cfd, &opCount, 1); //cfd로부터 최대 1바이트만 읽어서 opCount변수에 저장한다
+        recvfrom(cfd,&opCount,1,0,(struct sockaddr*)&cliaddr,&len);
         if(opCount <= 0) { 
             printf("Server close(%d)\n",opCount);
             close(cfd);
@@ -59,15 +52,16 @@ int main(int argc, char** argv) {
         printf("Operand count: %d\n", opCount); //그리고 operand count 출력
 
         for(int i=0; i<opCount; i++) { //입력한 opCount만큼 반복
-            read(cfd, &operand[i], 4); //클라이언트로소켓으로부터 4바이트씩 읽고 operand[i]에 저장, read는 이전에 읽은 데이터 다음 위치에서 계속 읽어서 이렇게 가능하다
+            //read(cfd, &operand[i], 4); //클라이언트로소켓으로부터 4바이트씩 읽고 operand[i]에 저장, read는 이전에 읽은 데이터 다음 위치에서 계속 읽어서 이렇게 가능하다
+            recvfrom(cfd,&operand[i],4,0,(struct sockaddr*)&cliaddr,&len);
             printf("Operand %d: %d\n", i, operand[i]); //i번째 operand 출력
         }
 
-        read(cfd, &operator, 1); //1바이트(char) 읽어서 operator에 저장
+        //read(cfd, &operator, 1); //1바이트(char) 읽어서 operator에 저장
+        recvfrom(cfd,&operator,1,0,(struct sockaddr*)&cliaddr,&len);
 
         opResult = operand[0];  //opResult에 첫번째 숫자 전달
         for(int i=0; i<opCount-1; i++) { //operator 빼고 반복
-            
             switch (operator) { //operator case에 따른 스위치문
             case '+':
                 opResult += operand[i+1]; //+일경우 opresult변수에 i+1 번째 숫자 덧셈
@@ -78,11 +72,13 @@ int main(int argc, char** argv) {
             case '*':
                 opResult *= operand[i+1];
             }
-            read(cfd, &operator, 1); //1바이트(char) 읽어서 operator에 저장
+            //read(cfd, &operator, 1); //1바이트(char) 읽어서 operator에 저장
+            recvfrom(cfd,&operator,1,0,(struct sockaddr*)&cliaddr,&len);
         }
 
         printf("Operation result: %d\n", opResult); 
-        write(cfd, &opResult, 4); //클라이언트 소켓에 opResult 전송
+        //write(cfd, &opResult, 4); //클라이언트 소켓에 opResult 전송
+        sendto(cfd, &opResult, 4, 0, (const struct sockaddr*)&cliaddr,len);
 
         close(cfd); //클라이언트 소켓 닫기
     }
