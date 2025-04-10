cad6487bec5363acb50b3e8492e5f90b3b7659ca
jihwan <yjh2353693@naver.com>
2025-04-10 21:02:38 +0900

    wait 주석

--- HW5/실습코드/wait.c
+++ HW5/실습코드/wait.c
@@ -8,22 +8,22 @@ int main(int argc, char *argv[])
 	int status;
 	pid_t pid=fork(); //fork 하면 현재 프로세스 복사
 	
-	if(pid==0) //자식 프로세스이면
+	if(pid==0) //자식 프로세스가 하는 행동
 	{
-		return 3;
+		return 3; //자식프로세스가 3을 반환하고 종료
 	}
-	else //부모 프로세스이면
+	else //부모 프로세스가 하는 행동
 	{
 		printf("Child PID: %d \n", pid);
-		pid=fork();
-		if(pid==0)
+		pid=fork(); //부모 프로세스에서 다시 fork
+		if(pid==0) //자식프로세스가 하는 행동
 		{
-			exit(7);
+			exit(7); //7을 반환하고 종료
 		}
-		else
+		else //부모프로세스가 하는 행동
 		{
-			printf("Child PID: %d \n", pid);
-			wait(&status);
+			printf("Child PID: %d \n", pid); 
+			wait(&status); //자식이 종료될때까지 block
 			printf("status: %d\n", status);
 			if(WIFEXITED(status))
 				printf("Child send one: %d \n", WEXITSTATUS(status));
