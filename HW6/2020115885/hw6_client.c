8ccb929b62a16c0f86082b8c3319f14b80021560
jihwan <yjh2353693@naver.com>
2025-04-27 18:36:20 +0900

    final commit

--- HW6/2020115885/hw6_client.c
+++ HW6/2020115885/hw6_client.c
@@ -11,20 +11,49 @@
 #define ID_SIZE 5
 void error_handling(char *message); 
 
+int sock; //소켓 선언
+//char message[MAX]; //메세지 배열 선언
+int str_len;
+struct sockaddr_in serv_adr;
+
+int opCount, opResult;
+
+struct iovec vec[3];
+char mode[MODE_SIZE];
+char id[ID_SIZE];
+char buf[MAX];
+
+void vec_init(){
+    vec[0].iov_base = mode;
+    vec[0].iov_len = MODE_SIZE;
+    vec[1].iov_base = id;
+    vec[1].iov_len = ID_SIZE;
+    vec[2].iov_base = buf;
+    vec[2].iov_len = MAX;
+}
+
+void caculator_input(){
+    printf("Operand count: ");
+    scanf("%d", &opCount); //operation Count를 입력받는다
+    buf[0] = (char)opCount; //buf배열 첫번째 인덱스에 opCount를 char형태로 저장
+
+    if(buf[0] <= 0) { //opCount가 음수면 소켓 종료
+        printf("Overflow will happen(%d)\n", opCount);
+        close(sock);
+        return ;
+    }
+    for(int i=0; i<opCount; i++) { //operation Count만큼 반복
+        printf("Operand %d: ", i); 
+        scanf("%d", (int*)&buf[(i*4)+1]); //숫자를 입력받아 int형태로 4바이트 단위로 저장
+    }
+    for(int i=0; i<opCount-1; i++) {
+        printf("Operator %d: ",i);
+        scanf(" %c", &buf[(opCount*4)+1+i]); //operator를 buf배열의 숫자개수x4+1+i 인덱스에 opCount입력받기(buf마지막에 operator들 넣기)
+    }
+}
+
 int main(int argc, char *argv[])
 {
-	int sock; //소켓 선언aabb
-	//char message[MAX]; //메세지 배열 선언
-	int str_len;
-	struct sockaddr_in serv_adr;
-
-    int opCount, opResult;
-
-    struct iovec vec[3];
-    char mode[MODE_SIZE];
-    char id[ID_SIZE];
-    char buf[MAX];
-
 	if(argc!=3) {
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
@@ -39,25 +68,27 @@ int main(int argc, char *argv[])
 	serv_adr.sin_addr.s_addr=inet_addr(argv[1]); 
 	serv_adr.sin_port=htons(atoi(argv[2]));
 
-    //Mode출력 및 입력받기
-    printf("Mode: ");
-    scanf("%s", mode);
-    if (strcmp(mode, "save") != 0 && strcmp(mode, "load") != 0 && strcmp(mode, "quit") != 0) {
-        printf("supported mode: save load quit\n");
-        close(sock);
-        return 0;
-    }
+    vec_init();
 
+    //Mode출력 및 입력받기
+    void mode_check();{
+        printf("Mode: ");
+        scanf("%s", mode);
+        if (strcmp(mode, "save") != 0 && strcmp(mode, "load") != 0 && strcmp(mode, "quit") != 0) {
+            printf("supported mode: save load quit\n");
+            close(sock);
+            return 0;
+        }
+    }
 
 	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1) //서버와 연결
 		error_handling("connect() error!");
 	else
 		puts("Connected....");
-    
+
+    //quit일때 수행
     if (!strcmp(mode, "quit")) {
-        puts("quit");
-        vec[0].iov_base = mode;
-        vec[0].iov_len = MODE_SIZE;
+        puts("quit");;
         writev(sock, vec, 1); //server로 write
         close(sock);
         return 0;
@@ -72,11 +103,8 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    //load일 때 수행
     if (!strcmp(mode, "load")) {
-        vec[0].iov_base = mode;
-        vec[0].iov_len = MODE_SIZE;
-        vec[1].iov_base = id;
-        vec[1].iov_len = ID_SIZE;
         writev(sock, vec, 2); //server로 write
         str_len = read(sock, buf, MAX-1); //server소켓에서 buf에 데이터를 읽어오기
         buf[str_len] = 0;
@@ -85,39 +113,12 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    //save일 경우 수행
-    printf("Operand count: ");
-    scanf("%d", &opCount); //operation Count를 입력받는다
-    buf[0] = (char)opCount; //buf배열 첫번째 인덱스에 opCount를 char형태로 저장
-
-    if(buf[0] <= 0) { //opCount가 음수면 소켓 종료
-        printf("Overflow will happen(%d)\n", opCount);
-        close(sock);
-        return 0;
-    }
-
-    for(int i=0; i<opCount; i++) { //operation Count만큼 반복
-        printf("Operand %d: ", i); 
-        scanf("%d", (int*)&buf[(i*4)+1]); //숫자를 입력받아 int형태로 4바이트 단위로 저장
-    }
-
-    for(int i=0; i<opCount-1; i++) {
-        printf("Operator %d: ",i);
-        scanf(" %c", &buf[(opCount*4)+1+i]); //operator를 buf배열의 숫자개수x4+1+i 인덱스에 opCount입력받기(buf마지막에 operator들 넣기)
-    }
-
-    vec[0].iov_base = mode;
-    vec[0].iov_len = MODE_SIZE;
-    vec[1].iov_base = id;
-    vec[1].iov_len = ID_SIZE;
-    vec[2].iov_base = buf;
-    vec[2].iov_len = MAX;
-
+    //save일 때 수행
+    caculator_input();
     //write(sock, buf, 1+(opCount*4)+(opCount-1)); //server소켓에 buf의 데이터를 전달
     writev(sock, vec, 3); //server로 write
     read(sock, &opResult, 4); //server소켓에서 데이터를 읽어와서 opResult에 저장
     printf("Operation result: %d\n", opResult);
-
 	close(sock);
 	return 0;
 }
--- HW6/2020115885/hw6_server.c
+++ HW6/2020115885/hw6_server.c
@@ -16,37 +16,59 @@
 #define BUF_SIZE 1024
 #define REQ_SIZE 100
 #define DATA_MAX 100
-void error_handling(char *buf);
-void read_childproc(int sig);
 
-int main(int argc, char *argv[])
+void read_childproc(int sig) {
+    pid_t pid;
+    int status;
+    pid = waitpid(-1, &status, WNOHANG);
+    printf("removed proc id: %d \n", pid);
+  }
+  
+  void error_handling(char *buf)
+  {
+      fputs(buf, stderr);
+      fputc('\n', stderr);
+      exit(1);
+  }
+
+int serv_sock, clnt_sock; //서버와 클라이언트 소켓 선언
+struct sockaddr_in serv_adr, clnt_adr;
+struct timeval timeout; //select 대기시간용
+fd_set reads, cpy_reads; //select용 fd_set형 집합, 원본과 복사본
+
+//계산기 변수들
+char opCount;
+int opResult;
+int operand[BUF_SIZE];
+char operator[BUF_SIZE]; //원래 단일 문자변수인데 여기선 배열로 선언함
+
+socklen_t adr_sz; //주소 사이즈 구조체
+int fd_max, str_len, fd_num, i; //가장 큰 fd번호, 읽은데이터길이
+char buf[BUF_SIZE];
+
+pid_t pid;
+int state;
+struct sigaction act;
+int fdsA[2], fdsB[2];
+
+//mode와 id
+struct iovec vec[3];
+char mode[MOD_SIZE]; // mode ("save", "load", "quit")
+char id[ID_SIZE];   // id (길이 4)
+char result[BUF_SIZE]; // 계산 데이터
+
+//save와 load에 사용할 id와 result 담을 구조체
+typedef struct data
 {
-	int serv_sock, clnt_sock; //서버와 클라이언트 소켓 선언
-	struct sockaddr_in serv_adr, clnt_adr;
-	struct timeval timeout; //select 대기시간용
-	fd_set reads, cpy_reads; //select용 fd_set형 집합, 원본과 복사본
+    char id[ID_SIZE];
+    char req[REQ_SIZE];
+}data;
+data dataArray[DATA_MAX];
 
-    //계산기 변수들
-    char opCount;
-    int opResult;
-    int operand[BUF_SIZE];
-    char operator[BUF_SIZE]; //원래 단일 문자변수인데 여기선 배열로 선언함
+int cnt = 0;
+char load_buf[BUF_SIZE];
 
-	socklen_t adr_sz; //주소 사이즈 구조체
-	int fd_max, str_len, fd_num, i; //가장 큰 fd번호, 읽은데이터길이
-	char buf[BUF_SIZE];
-
-	pid_t pid;
-	int state;
-	struct sigaction act;
-	int fdsA[2], fdsB[2];
-
-    //mode와 id
-    struct iovec vec[3];
-    char mode[MOD_SIZE]; // mode ("save", "load", "quit")
-    char id[ID_SIZE];   // id (길이 4)
-    char result[BUF_SIZE]; // 계산 데이터
-
+void vec_init(){
     // 각 버퍼 설정
     vec[0].iov_base = mode;
     vec[0].iov_len = MOD_SIZE;
@@ -54,18 +76,122 @@ int main(int argc, char *argv[])
     vec[1].iov_len = ID_SIZE;
     vec[2].iov_base = result;
     vec[2].iov_len = BUF_SIZE;
+}
 
-    //save와 load에 사용할 id와 result 담을 구조체
-    typedef struct data
-    {
-        char id[ID_SIZE];
-        char req[REQ_SIZE];
-    }data;
+void calculator(){
+    //read(i, &opCount, 1); //cfd로부터 최대 1바이트만 읽어서 opCount변수에 저장한다
+    opCount = result[0];  // opCount 저장
+    printf("Operand count: %d\n", opCount); //그리고 operand count 출력
 
-    data dataArray[DATA_MAX];
+    for(int j=0; j<opCount; j++) { //입력한 opCount만큼 반복
+        //read(i, &operand[j], 4); //클라이언트로소켓으로부터 4바이트씩 읽고 operand[i]에 저장, read는 이전에 읽은 데이터 다음 위치에서 계속 읽어서 이렇게 가능하다
+        memcpy(&operand[j], &result[1 + j*4], 4);  // 4바이트씩 operand 복원
+        printf("Operand %d: %d\n", j, operand[j]); //j번째 operand 출력
+    }
 
+    opResult = operand[0];  //opResult에 첫번째 숫자 전달
+    sprintf(buf , "%d", operand[0]);  // 시작:  첫 번째 숫자
+
+    for(int j=0; j<opCount-1; j++) { //operator 빼고 반복
+        //read(clnt_sock, &operator, 1); //1바이트(char) 읽어서 operator에 저장
+        operator[j] = result[1 + opCount*4 + j];  // 그 뒤에는 operator들
+        switch (operator[j]) { //operator case에 따른 스위치문
+        case '+':
+            opResult += operand[j+1]; //+일경우 opresult변수에 i+1 번째 숫자 덧셈
+            break;
+        case '-':
+            opResult -= operand[j+1];
+            break;
+        case '*':
+            opResult *= operand[j+1];
+        }
+        sprintf(buf + strlen(buf), "%c%d", operator[j], operand[j+1]); //buf에 연산자랑 다음 operand 이어붙이기
+    }
+}
+
+void store_loop(){
     int cnt = 0;
-    char load_buf[BUF_SIZE];
+    while(1)
+    {
+        read(fdsA[0], mode, MOD_SIZE); //부모프로세스로부터 mode값 식별
+
+        if(strcmp(mode, "save") == 0 ){ 
+            read(fdsA[0], &dataArray[cnt], sizeof(dataArray[cnt])); // id와 결과값 dataArray구조체로 받아서 저장
+            cnt++;
+        }
+        else if(strcmp(mode, "load") == 0 ){
+            read(fdsA[0], id, ID_SIZE); //부모프로세스로부터 id값 식별
+            memset(result, 0 ,sizeof(result)); //result배열 초기화
+            for (int i = 0; i < cnt; i++){  //dayaArray 개수만큼 순환
+                if(strcmp(id, dataArray[i].id) == 0 ){ //받아온 id와 dataArray에 있는id가 일치하면
+                    sprintf(load_buf, "%s: %s", dataArray[i].id, dataArray[i].req); //load_buf에 id: result 꼴로 저장
+                    strcat(result, load_buf); //result에 이어붙히기
+                }
+            }
+            if (result[0]){ //result에 문자가 존재하면
+                write(fdsB[1], result, strlen(result));
+            }
+            else{ //result에 아무것도 없으면
+                char not_exist[BUF_SIZE] = "Not exist\n";
+                write(fdsB[1], not_exist, strlen(not_exist));
+            }
+        }
+        else if(strcmp(mode, "quit") == 0 ){
+            close(serv_sock);
+            return;
+        }
+    }
+}
+
+void mode_check(){
+    if (strcmp(mode, "save") == 0){
+        write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
+
+        calculator();
+        
+        printf("Operation result: %d\n", opResult);
+        write(i, &opResult, 4); //클라이언트 소켓에 opResult 전송
+
+        sprintf(buf + strlen(buf), "=%d\n", opResult); //buf에 opResult까지 붙이기
+
+        strcpy(dataArray[cnt].id, id);    // id 저장
+        strcpy(dataArray[cnt].req, buf);  // 계산 수식 저장
+        write(fdsA[1], &dataArray[cnt], sizeof(dataArray[cnt]));
+        cnt++;
+        printf("save to %s\n", id);
+
+        close(i);
+        FD_CLR(i, &reads);
+        printf("closed client: %d \n", i);
+    }
+    else if (strcmp(mode, "load") == 0) {
+        printf("load from %s\n", id);
+        write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
+        write(fdsA[1], id, ID_SIZE); //id정보 자식프로세스로 보내기
+
+        str_len = read(fdsB[0], load_buf, sizeof(load_buf)); //자식프로세스로부터 result 문자열 읽어서 load_buf에 저장
+
+        write(i, load_buf, str_len); //현재 파일디스크립터에 result 문자열 보내기
+        
+        close(i);
+        FD_CLR(i, &reads);
+        printf("closed client: %d \n", i);
+    }
+    else if (strcmp(mode, "quit") == 0) {
+        write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
+        puts("quit");
+
+        close(i);
+        FD_CLR(i, &reads);
+        printf("closed client: %d \n", i);
+        close(serv_sock);
+        return ;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    vec_init();
 
 	if(argc!=2) {
 		printf("Usage : %s <port>\n", argv[0]);
@@ -76,7 +202,6 @@ int main(int argc, char *argv[])
 	sigemptyset(&act.sa_mask);
 	act.sa_flags=0;
 	state=sigaction(SIGCHLD, &act, 0); //child가 죽었을때 핸들러 실행
-
 
 	serv_sock=socket(PF_INET, SOCK_STREAM, 0); //소켓 만들기
 	memset(&serv_adr, 0, sizeof(serv_adr));
@@ -97,38 +222,7 @@ int main(int argc, char *argv[])
 	pid=fork();
 	if(pid==0) //자식프로세스는
 	{
-        int cnt = 0;
-		while(1)
-		{
-            read(fdsA[0], mode, MOD_SIZE); //부모프로세스로부터 mode값 식별
-
-            if(strcmp(mode, "save") == 0 ){ 
-                read(fdsA[0], &dataArray[cnt], sizeof(dataArray[cnt])); // id와 결과값 dataArray구조체로 받아서 저장
-                cnt++;
-            }
-            else if(strcmp(mode, "load") == 0 ){
-                read(fdsA[0], id, ID_SIZE); //부모프로세스로부터 id값 식별
-                memset(result, 0 ,sizeof(result)); //result배열 초기화
-                for (int i = 0; i < cnt; i++){  //dayaArray 개수만큼 순환
-                    if(strcmp(id, dataArray[i].id) == 0 ){ //받아온 id와 dataArray에 있는id가 일치하면
-                        sprintf(load_buf, "%s: %s", dataArray[i].id, dataArray[i].req); //load_buf에 id: result 꼴로 저장
-                        strcat(result, load_buf); //result에 이어붙히기
-                    }
-                }
-                if (result[0]){ //result에 문자가 존재하면
-                    write(fdsB[1], result, strlen(result));
-                }
-                else{ //result에 아무것도 없으면
-                    char not_exist[BUF_SIZE] = "Not exist\n";
-                    write(fdsB[1], not_exist, strlen(not_exist));
-                }
-
-            }
-
-
-
-
-		}
+        store_loop();
 		return 0;
 	}
 
@@ -172,97 +266,12 @@ int main(int argc, char *argv[])
 					}
 					else
 					{
-                        if (strcmp(mode, "save") == 0){
-                            write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
-                            //read(i, &opCount, 1); //cfd로부터 최대 1바이트만 읽어서 opCount변수에 저장한다
-                            opCount = result[0];  // opCount 저장
-
-                            printf("Operand count: %d\n", opCount); //그리고 operand count 출력
-                    
-                            for(int j=0; j<opCount; j++) { //입력한 opCount만큼 반복
-                                //read(i, &operand[j], 4); //클라이언트로소켓으로부터 4바이트씩 읽고 operand[i]에 저장, read는 이전에 읽은 데이터 다음 위치에서 계속 읽어서 이렇게 가능하다
-                                memcpy(&operand[j], &result[1 + j*4], 4);  // 4바이트씩 operand 복원
-                                printf("Operand %d: %d\n", j, operand[j]); //j번째 operand 출력
-                            }
-                    
-                            opResult = operand[0];  //opResult에 첫번째 숫자 전달
-                            
-                            sprintf(buf , "%d", operand[0]);  // 시작:  첫 번째 숫자
-                
-                            for(int j=0; j<opCount-1; j++) { //operator 빼고 반복
-                                //read(clnt_sock, &operator, 1); //1바이트(char) 읽어서 operator에 저장
-                                operator[j] = result[1 + opCount*4 + j];  // 그 뒤에는 operator들
-                                switch (operator[j]) { //operator case에 따른 스위치문
-                                case '+':
-                                    opResult += operand[j+1]; //+일경우 opresult변수에 i+1 번째 숫자 덧셈
-                                    break;
-                                case '-':
-                                    opResult -= operand[j+1];
-                                    break;
-                                case '*':
-                                    opResult *= operand[j+1];
-                                }
-                                sprintf(buf + strlen(buf), "%c%d", operator[j], operand[j+1]); //buf에 연산자랑 다음 operand 이어붙이기
-                            }
-                            
-                            printf("Operation result: %d\n", opResult);
-                            write(i, &opResult, 4); //클라이언트 소켓에 opResult 전송
-                
-                            sprintf(buf + strlen(buf), "=%d\n", opResult); //buf에 opResult까지 붙이기
-
-                            strcpy(dataArray[cnt].id, id);    // id 저장
-                            strcpy(dataArray[cnt].req, buf);  // 계산 수식 저장
-                            write(fdsA[1], &dataArray[cnt], sizeof(dataArray[cnt]));
-                            cnt++;
-                            printf("save to %s\n", id);
-
-                            close(i);
-                            FD_CLR(i, &reads);
-                            printf("closed client: %d \n", i);
-                        }
-                        else if (strcmp(mode, "load") == 0) {
-                            printf("load from %s\n", id);
-                            write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
-                            write(fdsA[1], id, ID_SIZE); //id정보 자식프로세스로 보내기
-
-                            str_len = read(fdsB[0], load_buf, sizeof(load_buf)); //자식프로세스로부터 result 문자열 읽어서 load_buf에 저장
-
-                            write(i, load_buf, str_len); //현재 파일디스크립터에 result 문자열 보내기
-                            
-                            close(i);
-                            FD_CLR(i, &reads);
-                            printf("closed client: %d \n", i);
-                        }
-                        else if (strcmp(mode, "quit") == 0) {
-                            write(fdsA[1], mode, MOD_SIZE); //mode정보 자식프로세스로 보내기
-                            puts("quit");
-
-                            close(i);
-                            FD_CLR(i, &reads);
-                            printf("closed client: %d \n", i);
-                        }
-
-                        
-					
+                        mode_check();
 					}
 				}
 			}
 		}
 	}
-	close(serv_sock);
-	return 0;
-}
-
-void read_childproc(int sig) {
-  pid_t pid;
-  int status;
-  pid = waitpid(-1, &status, WNOHANG);
-  printf("removed proc id: %d \n", pid);
-}
-
-void error_handling(char *buf)
-{
-	fputs(buf, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    close(serv_sock);
+    return 0;
 }
